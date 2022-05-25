455ef2808ffc37ba3814ac1df6434fca34e9c531
Kateryna_Rozhkovska <kateryna_rozhkovska@epam.com>
2022-05-25 20:01:48 +0300

    Add homework for Reflection

---
+++ src/main/java/MyAnnotationTest.java
@@ -0,0 +1,11 @@
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface MyAnnotationTest {
+    String message();
+}
---
+++ src/main/java/Shop.java
@@ -0,0 +1,31 @@
+@MyAnnotationTest(message = "Kate1")
+public class Shop {
+    private int number;
+    private String phoneNumber;
+    private String directorName;
+    private String address;
+    private String shopName;
+
+
+    private Shop(int numberOfShop, String address, String shopName, String phoneNumber, String directorName) {
+        this.number = numberOfShop;
+        this.address = address;
+        this.shopName = shopName;
+        this.phoneNumber = phoneNumber;
+        this.directorName = directorName;
+    }
+
+    public Shop() {
+    }
+
+    private void methodOne() {
+    }
+
+    private String methodTwo() {
+        return null;
+    }
+
+    private static int methodTree() {
+        return 0;
+    }
+}
